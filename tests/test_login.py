54975ed559b2305f99c2b9e95c526be0c6b65199
Akar57 <shivaprasadm393@gmail.com>
2026-09-10 20:59:17 +0530

    removed

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,30 @@
+import os
+
+from dotenv import load_dotenv
 from playwright.sync_api import Page, expect
 
 from pages.login_page import LoginPage
 
 
+load_dotenv()
+
+
 def test_valid_login(page: Page, base_url: str):
+    email = os.getenv("GHOST_EMAIL")
+    password = os.getenv("GHOST_PASSWORD")
+
+    if not email or not password:
+        raise RuntimeError(
+            "GHOST_EMAIL and GHOST_PASSWORD must be set."
+        )
+
     login = LoginPage(page)
 
     login.open(base_url)
+
     login.login(
-        "james@gmail.com",
-        "1234567899"
+        email,
+        password
     )
 
     expect(page).not_to_have_url(
